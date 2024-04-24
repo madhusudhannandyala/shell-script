27cfaca1a61092a9e55be7637f487695e0ca9f86
msn <msn@gmail.com>
2024-04-24 18:17:40 +0530

    hello script

--- 01-spl-var.sh
+++ 01-spl-var.sh
@@ -3,3 +3,8 @@
 echo "all varibale : $@"
 echo "number of variables passed: $#"
 echo "script name : $0"
+echo "cureent working directory: $PWD"
+echo "HOME  directory of cureent user: $HOME"
+echo "which user is running this script: $USER"
+echo "hostname: $HOSTNAME"
+echo "processid of the current shell script: $$"
