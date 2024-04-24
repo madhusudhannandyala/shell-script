e74ec9c643671646ed497c4027d396a9db56414b
msn <msn@gmail.com>
2024-04-24 20:11:23 +0530

    hello script

---
+++ 05-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]
+then
+
+    echo "this will run only with root access only"
+else
+    echo "you are in root access"
+fi
+
+dnf install mysql -y
