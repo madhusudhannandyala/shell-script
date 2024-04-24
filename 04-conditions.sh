8a63a79a8d689842b4b71ebc24344d0e46a20d07
msn <msn@gmail.com>
2024-04-24 19:32:09 +0530

    hello script

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -6,7 +6,7 @@ if [ $NUMBER -gt 10 ]
 
 then
 
-    echo "given number $NUMBER is greater than is"
+    echo "given number $NUMBER is greater than is 10"
 
 else
     echo  "given $NUMBER is less than 10"
